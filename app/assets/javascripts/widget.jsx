c79bde01ec31a8367b7c8dcafdde0cbf0900f354
Connor Weng <weng@icbc.com>
2015-12-26 22:53:43 +0800

    fix code style

--- app/assets/javascripts/widget.jsx
+++ app/assets/javascripts/widget.jsx
@@ -2,7 +2,7 @@ var WidgetFooter = React.createClass({
   render: function() {
     return (
       <div className="box-footer text-center">
-        <a className="uppercase" href="#">查看详情</a>
+          <a className="uppercase" href="#">查看详情</a>
       </div>
     );
   }
@@ -12,26 +12,26 @@ var WidgetBody = React.createClass({
   render: function() {
     return (
       <div className="box-body">
-        <div className="table-responsive">
-          <table className="table no-margin">
-            <thead>
-              <tr>
-                <th>模块</th>
-                <th>总用时</th>
-                <th>人员</th>
-                <th>事件</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>test text</td>
-                <td>test text</td>
-                <td>test text</td>
-                <td>test text</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+          <div className="table-responsive">
+              <table className="table no-margin">
+                  <thead>
+                      <tr>
+                          <th>模块</th>
+                          <th>总用时</th>
+                          <th>人员</th>
+                          <th>事件</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>test text</td>
+                          <td>test text</td>
+                          <td>test text</td>
+                          <td>test text</td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
       </div>
     );
   }
@@ -41,15 +41,15 @@ var WidgetHeader = React.createClass({
   render: function() {
     return (
       <div className="box-header with-border">
-        <h3 className="box-title">{this.props.title}</h3>
-        <div className="box-tools pull-right">
-          <button className="btn btn-box-tool" type="button" data-widget="collapse">
-            <i className="fa fa-minus"></i>
-          </button>
-          <button className="btn btn-box-tool" type="button" data-widget="remove">
-            <i className="fa fa-times"></i>
-          </button>
-        </div>
+          <h3 className="box-title">{this.props.title}</h3>
+          <div className="box-tools pull-right">
+              <button className="btn btn-box-tool" type="button" data-widget="collapse">
+                  <i className="fa fa-minus"></i>
+              </button>
+              <button className="btn btn-box-tool" type="button" data-widget="remove">
+                  <i className="fa fa-times"></i>
+              </button>
+          </div>
       </div>
     );
   }
@@ -59,9 +59,9 @@ var Widget = React.createClass({
   render: function() {
     return (
       <div className="box box-danger">
-        <WidgetHeader title={this.props.title} />
-        <WidgetBody />
-        <WidgetFooter />
+          <WidgetHeader title={this.props.title} />
+          <WidgetBody />
+          <WidgetFooter />
       </div>
     );
   }
