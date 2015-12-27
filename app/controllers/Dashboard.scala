a7d18ea19fb64435a89fc4547d67cdfe03ddfef4
Connor Weng <weng@icbc.com>
2015-12-27 16:52:24 +0800

    rename core to dashboard

--- app/controllers/Dashboard.scala
+++ app/controllers/Dashboard.scala
@@ -5,7 +5,7 @@ import play.api.mvc.{Action, Controller}
 /**
   * Created by ConnorWeng on 2015/12/21.
   */
-class Core extends Controller {
+class Dashboard extends Controller {
 
   def index = Action {
     Ok(views.html.dashboard())
