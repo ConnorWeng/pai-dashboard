045b229e7b7fb5f2c31f73d93557f3d6b24d9350
Connor Weng <foamtea30@gmail.com>
2016-01-24 00:16:45 +0800

    translate to Chinese

--- app/assets/javascripts/dashboard.jsx
+++ app/assets/javascripts/dashboard.jsx
@@ -88,7 +88,7 @@ var Dashboard = React.createClass({
               <div className="col-xs-12 col-md-4">
                   <div className="box box-default">
                       <div className="box-header with-border">
-                          <h3 className="box-title">Browser Usage</h3>
+                          <h3 className="box-title">浏览器</h3>
                           <div className="box-tools pull-right">
                               <button type="button" className="btn btn-box-tool" data-widget="collapse"><i className="fa fa-minus"></i>
                               </button>
@@ -104,23 +104,23 @@ var Dashboard = React.createClass({
                               </div>
                               <div className="col-md-4">
                                   <ul className="chart-legend clearfix">
+                                      <li><i className="fa fa-circle-o text-gray"></i> IE6</li>
+                                      <li><i className="fa fa-circle-o text-green"></i> IE8</li>
+                                      <li><i className="fa fa-circle-o text-yellow"></i> IE11</li>
                                       <li><i className="fa fa-circle-o text-red"></i> Chrome</li>
-                                      <li><i className="fa fa-circle-o text-green"></i> IE</li>
-                                      <li><i className="fa fa-circle-o text-yellow"></i> FireFox</li>
                                       <li><i className="fa fa-circle-o text-aqua"></i> Safari</li>
-                                      <li><i className="fa fa-circle-o text-light-blue"></i> Opera</li>
-                                      <li><i className="fa fa-circle-o text-gray"></i> Navigator</li>
+                                      <li><i className="fa fa-circle-o text-light-blue"></i> FireFox</li>
                                   </ul>
                               </div>
                           </div>
                       </div>
                       <div className="box-footer no-padding">
                           <ul className="nav nav-pills nav-stacked">
-                              <li><a href="#">United States of America
+                              <li><a href="#">上海
                                   <span className="pull-right text-red"><i className="fa fa-angle-down"></i> 12%</span></a></li>
-                              <li><a href="#">India <span className="pull-right text-green"><i className="fa fa-angle-up"></i> 4%</span></a>
+                              <li><a href="#">珠海 <span className="pull-right text-green"><i className="fa fa-angle-up"></i> 4%</span></a>
                               </li>
-                              <li><a href="#">China
+                              <li><a href="#">北京
                                   <span className="pull-right text-yellow"><i className="fa fa-angle-left"></i> 0%</span></a></li>
                           </ul>
                       </div>
