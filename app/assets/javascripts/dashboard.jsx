5ac63d2f234b9f0d1ce8d7f181edbcb3b7c6d1bb
Connor Weng <foamtea30@gmail.com>
2016-01-23 17:27:53 +0800

    add some dummy metrics onto dashboard

--- app/assets/javascripts/dashboard.jsx
+++ app/assets/javascripts/dashboard.jsx
@@ -22,6 +22,56 @@ var Dashboard = React.createClass({
               </div>
           </div>
           <div className="row">
+              <div className="col-lg-3 col-xs-6">
+                  <div className="small-box bg-aqua">
+                      <div className="inner">
+                          <h3>150</h3>
+                          <p>Pages / Session</p>
+                      </div>
+                      <div className="icon">
+                          <i className="ion ion-bag"></i>
+                      </div>
+                      <a href="#" className="small-box-footer">详情 <i className="fa fa-arrow-circle-right"></i></a>
+                  </div>
+              </div>
+              <div className="col-lg-3 col-xs-6">
+                  <div className="small-box bg-green">
+                      <div className="inner">
+                          <h3>53<sup style={{fontSize: '20px'}}>%</sup></h3>
+                          <p>Bounce Rate</p>
+                      </div>
+                      <div className="icon">
+                          <i className="ion ion-stats-bars"></i>
+                      </div>
+                      <a href="#" className="small-box-footer">详情 <i className="fa fa-arrow-circle-right"></i></a>
+                  </div>
+              </div>
+              <div className="col-lg-3 col-xs-6">
+                  <div className="small-box bg-yellow">
+                      <div className="inner">
+                          <h3>44</h3>
+                          <p>Unique Visitors</p>
+                      </div>
+                      <div className="icon">
+                          <i className="ion ion-person-add"></i>
+                      </div>
+                      <a href="#" className="small-box-footer">详情 <i className="fa fa-arrow-circle-right"></i></a>
+                  </div>
+              </div>
+              <div className="col-lg-3 col-xs-6">
+                  <div className="small-box bg-red">
+                      <div className="inner">
+                          <h3>65</h3>
+                          <p>Page Views</p>
+                      </div>
+                      <div className="icon">
+                          <i className="ion ion-pie-graph"></i>
+                      </div>
+                      <a href="#" className="small-box-footer">详情 <i className="fa fa-arrow-circle-right"></i></a>
+                  </div>
+              </div>
+          </div>
+          <div className="row">
               <div className="col-xs-12 col-md-8">
                   <div className="row">
                       <div className="col-xs-12" id="web-modules-widget">
