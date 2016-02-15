6d61bd5eaf87532fdf973ee79602ca2e5726c252
Connor Weng <foamtea30@gmail.com>
2016-02-15 16:32:44 +0800

    implement visitorOverviewDaily

--- app/controllers/Visitor.scala
+++ app/controllers/Visitor.scala
@@ -16,9 +16,24 @@ class Visitor @Inject()(archiveNumericDAO: ArchiveNumericDAO) extends Controller
   }
 
   def visitors(appId: Int, startDate: String, endDate: String) = Action.async {
-    archiveNumericDAO.all(appId, startDate, endDate).map( rs =>
+    archiveNumericDAO.all(appId, startDate, endDate).map(rs =>
       Ok(Json.toJson(rs))
     )
   }
 
+  def visitorOverviewDaily(appId: Int, startDate: String, endDate: String) = Action.async {
+    archiveNumericDAO.visitorOverviewDaily(appId, startDate, endDate).map { rs =>
+      val pps = for (i <- rs._3.indices) yield {
+        if (rs._3(i) != 0) {
+          rs._1(i).toFloat / rs._3(i).toFloat
+        } else {
+          0
+        }
+      }
+      Ok(Json.obj(
+        ("pageview", Json.toJson(rs._1)),
+        ("unique_visitor", Json.toJson(rs._2)),
+        ("pageview_per_session", Json.toJson(pps))))
+    }
+  }
 }
--- app/dao/ArchiveNumericDAO.scala
+++ app/dao/ArchiveNumericDAO.scala
@@ -1,16 +1,21 @@
 package dao
 
 import java.sql.Date
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 import com.google.inject.Inject
 import models.Visitors
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
 
+import scala.collection.mutable
+
 /**
   * Created by Connor on 2/14/16.
   */
 class ArchiveNumericDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
+
   import driver.api._
 
   def all(appId: Int, startDate: String, endDate: String) = {
@@ -21,7 +26,7 @@ class ArchiveNumericDAO @Inject()(protected val dbConfigProvider: DatabaseConfig
       .filter(_.date2 <= Date.valueOf(endDate))
       .groupBy(r => (r.appId, r.name))
       .map { case (key, values) =>
-          (key._1, key._2, values.map(_.value).sum)
+        (key._1, key._2, values.map(_.value).sum)
       }
 
     import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -39,5 +44,46 @@ class ArchiveNumericDAO @Inject()(protected val dbConfigProvider: DatabaseConfig
       }
       Visitors(appId, pageViews, session, 0, uniqueVisitor, 0)
     }
+  }
+
+  def visitorOverviewDaily(appId: Int, startDate: String, endDate: String) = {
+    val query = Tables.ArchiveNumeric
+      .filter(_.appId === appId)
+      .filter(_.period === 1.toByte)
+      .filter(_.date1 >= Date.valueOf(startDate))
+      .filter(_.date2 <= Date.valueOf(endDate))
+
+    import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+    db.run(query.result).map { rows =>
+      val dates = dateList(startDate, endDate)
+      val pageViewsDaily = new Array[Int](dates.size)
+      val uniqueVisitorDaily = new Array[Int](dates.size)
+      val sessionDaily = new Array[Int](dates.size)
+      val dateFormat = new SimpleDateFormat("yyyyMMdd")
+
+      rows.foreach { row =>
+        val dateString = dateFormat.format(row.date1.get)
+        row.name match {
+          case "pageview" => pageViewsDaily(dates.indexOf(dateString)) = row.value.getOrElse(0.0).toInt
+          case "uniquevisitor" => uniqueVisitorDaily(dates.indexOf(dateString)) = row.value.getOrElse(0.0).toInt
+          case "session" => sessionDaily(dates.indexOf(dateString)) = row.value.getOrElse(0.0).toInt
+        }
+      }
+
+      (pageViewsDaily.toList, uniqueVisitorDaily.toList, sessionDaily.toList)
+    }
+  }
+
+  def dateList(startDate: String, endDate: String) = {
+    val startCalendar = new Calendar.Builder().setInstant(Date.valueOf(startDate)).build()
+    val endCalendar = new Calendar.Builder().setInstant(Date.valueOf(endDate)).build()
+    val dates = mutable.Buffer[String]()
+    val dateFormat = new SimpleDateFormat("yyyyMMdd")
+    while (startCalendar.compareTo(endCalendar) < 0) {
+      dates += dateFormat.format(startCalendar.getTime)
+      startCalendar.add(Calendar.DAY_OF_YEAR, 1)
+    }
+    dates.toList
   }
 }
